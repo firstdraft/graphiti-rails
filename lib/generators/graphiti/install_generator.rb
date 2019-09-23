46817f6d142ce1b8a2a3cf32bcfd38a1d6c5213f
Logan Price <logandsprice@gmail.com>
2019-09-23 17:09:46 -0500

    Refactor installing routes

--- lib/generators/graphiti/install_generator.rb
+++ lib/generators/graphiti/install_generator.rb
@@ -52,20 +52,12 @@ module Graphiti
       end
 
       insert_into_file "config/routes.rb", after: "Rails.application.routes.draw do\n" do
-        if defined?(VandalUi)
           <<-STR
   scope path: ApplicationResource.endpoint_namespace, defaults: { format: :jsonapi } do
-    mount VandalUi::Engine, at: '/vandal'
-    # your routes go here
+    # your routes go here\
+#{"\n\t\tmount VandalUi::Engine, at: '/vandal'" if defined?(VandalUi)}
   end
           STR
-        else
-          <<-STR
-  scope path: ApplicationResource.endpoint_namespace, defaults: { format: :jsonapi } do
-    # your routes go here
-  end
-          STR
-        end
       end
     end
 
